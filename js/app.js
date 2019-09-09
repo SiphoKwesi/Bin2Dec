d4ca9c9007381c554a0f62e297f4546544e2fdb4
SiphoKwesi <54917663+SiphoKwesi@users.noreply.github.com>
2019-09-09 19:45:50 +0100

    Update app.js
    added a reset feature with the addition of a new button and reset function in the IIFE

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-
 (function () {
         "use strict";
 
 let input = document.getElementById("input");
 let output = document.getElementById("output");
-let myBtn = document.getElementsByClassName("button-convert");
+let myConvertBtn = document.getElementsByClassName("button-convert");
+let myResetBtn = document.getElementsByClassName("button-reset");
 let parsed;
 
 function convert(){
@@ -14,11 +14,17 @@ function convert(){
         display(parsed);
 } 
 
-
 function display(x) {
         output.value = x;
         return output.value;
 }
 
-myBtn[0].addEventListener("click", convert);
+function reset(){
+        input.value = "0";
+        output.value = "0";
+}
+
+myConvertBtn[0].addEventListener("click", convert);
+myResetBtn[0].addEventListener("click", reset);
+
 })();
